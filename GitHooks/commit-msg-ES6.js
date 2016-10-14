60f550b1bcc845872d47006c7b1c85e1e215e016
kymwei <kymwei1002@gmail.com>
2016-10-13 19:30:08 -0700

    Story: S-11111 | yea yea

---
+++ GitHooks/commit-msg-ES6.js
@@ -0,0 +1,69 @@
+'use strict';
+
+
+
+var validateCommitMsg = function (msg) {
+
+    const commitMessageReg = '(Story: S-|BugId: |Epic: E-)[0-9]* \| .*$';
+    const fs = require('fs');
+    const file = process.argv[2]; // commit message is stored in a file, usually ".git/COMMIT_EDITMSG";
+
+    if(!file){
+        console.error('ERROR: No commit message file found');
+        return 1;
+    }else{
+        let commitMsg = fs.readFileSync(file, { encoding: 'utf8' });
+
+        if (!RegExp(commitMessageReg).test(commitMsg)) {
+             console.error(`ERROR: Your commit message must follow this naming convention:
+                            Story: S-XXXXX | desc ,
+                            BugId: XXXXX | desc ,
+                            Epic: E-XXXXX | desc
+                            `);
+            return 1;
+        }
+        return 0;
+    }
+}
+
+var validateBranch = function (branchName) {
+
+    const protectedBranches =  ['master', 'staging', 'develop'];
+    const  childProcess = require('child_process');
+    const  spawn = childProcess.spawnSync('git', ['rev-parse', '--abbrev-ref', 'HEAD']);
+
+    let error = spawn.stderr.toString().trim();
+
+
+    if(error){
+        console.error('ERROR: Failed to get branch name');
+        return 1;
+    } else{
+
+        branchName = branchName.trim();
+
+        if (protectedBranches.join('').indexOf(branchName) >= 0) {
+           console.error(`ERROR: You cannot commit directly to the ${branchName} branch`);
+           return 1;
+       } elseP
+       return 0;
+    }
+
+}
+
+
+//should handel merge conflict ??
+//
+module.exports = {
+    validateCommit: function () {
+        let statusCode = 1;
+
+        if (validateCommitMsg() === 0 && validateBranch() === 0) {
+            statusCode = 0;
+        }
+        process.exit(statusCode);
+    },
+    validatePrepareCommit: function(){
+
+    }
+};
