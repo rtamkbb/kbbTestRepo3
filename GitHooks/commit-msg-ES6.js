c67cd972d511359355c1e54650ae71d64f36f74f
kymwei <kymwei1002@gmail.com>
2016-10-13 19:49:05 -0700

    Story: S-11111 | another test

--- GitHooks/commit-msg-ES6.js
+++ GitHooks/commit-msg-ES6.js
@@ -26,31 +26,67 @@ var validateCommitMsg = function (msg) {
     }
 }
 
-var validateBranch = function (branchName) {
+var validateBranch = function () {
 
     const protectedBranches =  ['master', 'staging', 'develop'];
     const  childProcess = require('child_process');
     const  spawn = childProcess.spawnSync('git', ['rev-parse', '--abbrev-ref', 'HEAD']);
 
     let error = spawn.stderr.toString().trim();
-
+    let currentBranchName = '';
 
     if(error){
         console.error('ERROR: Failed to get branch name');
         return 1;
     } else{
 
-        branchName = branchName.trim();
+        currentBranchName = spawn.stdout.toString().trim();
 
-        if (protectedBranches.join('').indexOf(branchName) >= 0) {
+        if (protectedBranches.join('').indexOf(currentBranchName) >= 0) {
            console.error(`ERROR: You cannot commit directly to the ${branchName} branch`);
            return 1;
-       } elseP
+       }
        return 0;
     }
 
 }
 
+var validateMergeToBranch = function () {
+
+    const protectedBranches =  ['master', 'staging', 'develop'];
+    const  childProcess = require('child_process');
+    const  spawn = childProcess.spawnSync('git', ['rev-parse', '--abbrev-ref', 'HEAD']);
+
+    let error = spawn.stderr.toString().trim();
+    let currentBranchName = '';
+    let mergeFromBranchName = '';
+
+    // get the source branch name
+    // I could only find out how to get the source head from the environment variables.  It's stored in the GITHEAD_<sha> variable
+    // Usually it can be found in the .git/MERGE_HEAD file, but that file is not availalble at this point.
+    for(var prop in process.env) {
+        if(prop.startsWith('GITHEAD_')) {
+            mergeFromBranchName = process.env[prop];
+            break;
+        }
+    }
+
+    if(error){
+        console.error('ERROR: Failed to get branch name');
+        return 1;
+    } else{
+
+        currentBranchName = branchName.trim();
+
+        if (protectedBranches.join('').indexOf(branchName) >= 0) {
+           console.error(`ERROR: You cannot commit directly to the ${branchName} branch`);
+           return 1;
+
+       } elseP
+       return 0;
+    }
+
+}
 
 //should handel merge conflict ??
 //
@@ -64,6 +100,11 @@ module.exports = {
         process.exit(statusCode);
     },
     validatePrepareCommit: function(){
+        let statusCode = 1;
 
+        if (validateCommitMsg() === 0 && validateMergeToBranch() === 0) {
+            statusCode = 0;
+        }
+        process.exit(statusCode);
     }
 };
